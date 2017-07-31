cb09af5f20a84d10a7c2ce7c56dd3e9064733e12
animalyu <animalyu@outlook.com>
2017-07-31 15:46:28 +0800

    31 July 2017
    Small changes.

--- Figure.m
+++ Figure.m
@@ -8,19 +8,26 @@ for i = 2:1:length(Times)
 end
 
 subplot(4,1,1);
-plot(Times,Acc); xlabel('Time (s) 10 Steps'); ylabel('Acc (g)');
-title('Figure 1: Acc Against Time While Running'); hold on;
+p = plot(Times,Acc,'k'); set(p,'LineWidth',1.5); grid on;
+xlabel('Time (s)','FontSize',15,'FontName','Times');
+ylabel('Acc_x (g)','FontSize',15,'FontName','Times');
+title('Figure 1: X axis of Acc','FontSize',18,'FontName','Times'); hold on;
 
 subplot(4,1,2);
-plot(Times,Gyro); xlabel('Times (s) 10 Steps'); ylabel('Angular Velocity (deg/s)');
-title('Figure 2: Angular Velocity Against Time While Running'); hold on;
+p = plot(Times,Gyro,'k'); set(p,'LineWidth',1.5); grid on;
+xlabel('Time (s)','FontSize',15,'FontName','Times');
+ylabel('Acc_y (g)','FontSize',15,'FontName','Times');
+title('Figure 2: Angle ','FontSize',18,'FontName','Times'); hold on;
 
 subplot(4,1,3);
-plot(Times,hx,Times,hy,Times,hz); xlabel('Time (s) 10 Steps'); ylabel('Magnetism (deg)');
-title('Figure 3: Magnetism Against Time While Running'); hold on;
+p = plot(Times,azg,'k'); set(p,'LineWidth',1.5); grid on;
+xlabel('Time (s)','FontSize',15,'FontName','Times');
+ylabel('Acc_z (g)','FontSize',15,'FontName','Times');
+title('Figure 3: Magnitude Against Time While Upstairs','FontSize',18,'FontName','Times'); hold on;
 
 subplot(4,1,4);
-plot(Times,PressurePa); xlabel('Time (s) 10 Steps'); ylabel('Pressure (Pa)');
-title('Figure 4: Pressure Against Time While Running'); hold on;
+p = plot(Times,PressurePa,'k'); set(p,'LineWidth',1.5); grid on;
+xlabel('Time (s) 10 Steps'); ylabel('Pressure (Pa)');
+title('Figure 4: Pressure Against Time While Upstairs'); hold on;
 
-%clear all
+clear all
---
+++ ThreeD.m
@@ -0,0 +1,4 @@
+for i = 1:1:181
+    plot3(axg(i), ayg(i), azg(i)); grid on; hold on;
+    pause(0.5);
+end
