e6a7523d66d5eda7302398564c53ac2bf8a22cfd
Tony-Jin <632719425@qq.com>
2017-08-10 15:16:11 +0800

    10/8/1017
    angle

--- Figure.m
+++ Figure.m
@@ -9,9 +9,9 @@ for i = 2:1:length(Times)
     Times(i) = Times(i-1) + 0.02;
 end
 
-%subplot(2,1,1);
-%plot(Times,axg,'*-',Times,ayg,'y',Times,azg,'b'); xlabel('Time (s) 10 Steps'); ylabel('Acc (g)');
-%title('Figure 1: Acc Against Time While Running'); hold on;
+subplot(2,1,1);
+plot(Times,Acc); xlabel('Time (s) 10 Steps'); ylabel('Acc (g)');
+title('Figure 1: Acc Against Time While Running'); hold on;
 
 %subplot(1,1,1);
 %plot(Times,Gyro,'*-'); xlabel('Times (s) 10 Steps'); ylabel('Angular Velocity (deg/s)');
@@ -25,13 +25,13 @@ end
 %plot(Times,hx,Times,hy,Times,hz); xlabel('Time (s) 10 Steps'); ylabel('Magnetism (deg)');
 %title('Figure 3: Magnetism Against Time While Running'); hold on;
 
-subplot(1,1,1);
-plot(Times,PressurePa,'*-'); xlabel('Time (s) 10 Steps'); ylabel('Pressure (Pa)');
-title('Figure 4: Pressure Against Time While Running'); hold on;
-
 %subplot(1,1,1);
-%plot(Times,AngleXdeg,'r',Times,AngleYdeg,'*-',Times,AngleZdeg,'b');
-%hold on;
+%plot(Times,PressurePa,'*-'); xlabel('Time (s) 10 Steps'); ylabel('Pressure (Pa)');
+%title('Figure 4: Pressure Against Time While Running'); hold on;
+
+subplot(2,1,2);
+plot(Times,AngleXdeg,'r',Times,AngleYdeg,'*-',Times,AngleZdeg,'b');
+hold on;
 
 
 %clear all
